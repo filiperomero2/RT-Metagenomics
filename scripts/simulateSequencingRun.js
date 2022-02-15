1d713e24eb9c50bb977422d99a53fffcd53d13eb
filiperomero2 <filiperomero2@gmail.com>
2022-02-15 12:58:21 +0000

    Adding structure to repo.

---
+++ scripts/simulateSequencingRun.js
@@ -0,0 +1,29 @@
+helpers = require('./helpers');
+
+const source = './test/';
+const destination = './data/';
+const files = helpers.listFiles(source);
+let counter = 0;
+
+helpers.createDir(destination);
+
+const simulateSequencingRun = () =>{
+    counter++;
+    const sample = files[Math.floor(Math.random() * files.length)];
+    const sourceFile = `${source}${sample}`;
+    const destinationFile = `${destination}${counter}_${sample}`;
+    helpers.copyFile(sourceFile,destinationFile);
+    console.log(`${sample} -> ${counter}`)
+    
+}
+
+const sleep = ms => new Promise(r => setTimeout(r, ms));
+
+executeLoop = async() =>{
+    while(true){
+        simulateSequencingRun(); 
+        await sleep(1000);
+    }
+}
+
+executeLoop();
