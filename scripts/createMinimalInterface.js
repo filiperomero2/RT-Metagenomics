b9585bde75924411b3c424234a122509f6a2f1ec
filiperomero2 <filiperomero2>
2022-03-31 15:32:51 +0100

    Added self-reload to page every 5 minutes

--- scripts/createMinimalInterface.js
+++ scripts/createMinimalInterface.js
@@ -6,6 +6,7 @@ const firstPart = `<!DOCTYPE html>
 <html>
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="refresh" content="300">
     <title>Metagenomics App</title>
     <link rel="stylesheet" href="reset.css">
     <link rel="stylesheet" href="style.css">
