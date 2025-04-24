c4e433907ca56f07c22c0ae9ab2e6fb0883c5b29
Felippe Nacif <felippenacif@gmail.com>
2025-04-24 16:14:22 -0300

    Remove unused SVG files and update global styles; add Meta page and navigation

---
+++ front/rt-metagenomics/src/app/meta/page.tsx
@@ -0,0 +1,7 @@
+export default function Meta() {
+  return (
+    <div>
+      <h1>Meta Page</h1>
+    </div>
+  );
+}
--- front/rt-metagenomics/src/app/page.tsx
+++ front/rt-metagenomics/src/app/page.tsx
@@ -1,11 +1,21 @@
+'use client'
+
+import './globals.css';
 import styles from "./page.module.css";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+
+  const handleMetaClick = () => {
+    router.push("/meta"); // Navigate to the "Meta" page
+  };
+
   return (
     <div className={styles.page}>
       <h1 className={styles.title}>Welcome to RT-Metagenomics</h1>
       <div className={styles.tilesContainer}>
-        <div className={styles.tile}>
+        <div className={styles.tile} onClick={handleMetaClick}>
           Meta
         </div>
         <div className={styles.tile}>
