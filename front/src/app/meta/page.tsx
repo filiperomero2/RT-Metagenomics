cc58d5a14d346652b5f03ef9e92cfa4a36724c7d
HiagoMM <hiagomarquesdemedeiros@gmail.com>
2025-05-14 19:38:43 -0300

    feat: add tailwind

--- front/src/app/meta/page.tsx
+++ front/src/app/meta/page.tsx
@@ -1,33 +1,35 @@
-'use client';
+"use client";
 
-import { useState } from 'react';
-import styles from './meta.module.css';
-import axios from 'axios';
+import { useState } from "react";
+import styles from "./meta.module.css";
+import axios from "axios";
 
 export default function Meta() {
   const [formData, setFormData] = useState({
-    dataType: '',
-    sampleSheet: '',
+    dataType: "",
+    sampleSheet: "",
     // configFile: '',
-    output: '',
-    runName: '',
+    output: "",
+    runName: "",
     trim: 0,
     // createConfigOnly: false,
     threads: 1,
     threadsTotal: 1,
-    kraken2Database: '',
-    kronaDatabase: '',
+    kraken2Database: "",
+    kronaDatabase: "",
     removeHumanReads: false,
     removeUnclassifiedReads: false,
-    adapters: '',
+    adapters: "",
     minimumReadLength: 50,
   });
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
     const { name, value, type, checked } = e.target;
     setFormData({
       ...formData,
-      [name]: type === 'checkbox' ? checked : value,
+      [name]: type === "checkbox" ? checked : value,
     });
   };
 
@@ -49,16 +51,17 @@ export default function Meta() {
     //   minimumReadLength:
     // }
 
-    axios.post('/v1/metagenomics', data)
-      .then(response => {
-        console.log('Response:', response.data);
+    axios
+      .post("/v1/metagenomics", data)
+      .then((response) => {
+        console.log("Response:", response.data);
         // Handle success
       })
-      .catch(error => {
-        console.error('Error:', error);
+      .catch((error) => {
+        console.error("Error:", error);
         // Handle error
       });
-    console.log('Form Data:', formData);
+    console.log("Form Data:", formData);
     // Handle form submission logic here
   };
 
@@ -69,7 +72,12 @@ export default function Meta() {
         <form className={styles.form} onSubmit={handleSubmit}>
           <label className={styles.label}>
             Data Type:
-            <select name="dataType" value={formData.dataType} onChange={handleChange} className={styles.input}>
+            <select
+              name="dataType"
+              value={formData.dataType}
+              onChange={handleChange}
+              className={styles.input}
+            >
               <option value="">Select</option>
               <option value="illumina">Illumina</option>
               <option value="nanopore">Nanopore</option>
@@ -77,7 +85,13 @@ export default function Meta() {
           </label>
           <label className={styles.label}>
             Sample Sheet:
-            <input type="file" name="sampleSheet" value={formData.sampleSheet} onChange={handleChange} className={styles.input} />
+            <input
+              type="file"
+              name="sampleSheet"
+              value={formData.sampleSheet}
+              onChange={handleChange}
+              className={styles.input}
+            />
           </label>
           {/* <label className={styles.label}>
             Config File:
@@ -85,15 +99,33 @@ export default function Meta() {
           </label> */}
           <label className={styles.label}>
             Output Directory:
-            <input type="file" name="output" value={formData.output} onChange={handleChange} className={styles.input} />
+            <input
+              type="file"
+              name="output"
+              value={formData.output}
+              onChange={handleChange}
+              className={styles.input}
+            />
           </label>
           <label className={styles.label}>
             Run Name:
-            <input type="text" name="runName" value={formData.runName} onChange={handleChange} className={styles.input} />
+            <input
+              type="text"
+              name="runName"
+              value={formData.runName}
+              onChange={handleChange}
+              className={styles.input}
+            />
           </label>
           <label className={styles.label}>
             Trim:
-            <input type="number" name="trim" value={formData.trim} onChange={handleChange} className={styles.input} />
+            <input
+              type="number"
+              name="trim"
+              value={formData.trim}
+              onChange={handleChange}
+              className={styles.input}
+            />
           </label>
           {/* <label className={styles.label}>
             Create Config Only:
@@ -101,41 +133,90 @@ export default function Meta() {
           </label> */}
           <label className={styles.label}>
             Threads:
-            <input type="number" name="threads" value={formData.threads} onChange={handleChange} className={styles.input} />
-          </label><label className={styles.label}>
+            <input
+              type="number"
+              name="threads"
+              value={formData.threads}
+              onChange={handleChange}
+              className={styles.input}
+            />
+          </label>
+          <label className={styles.label}>
             Threads Total:
-            <input type="number" name="threadsTotal" value={formData.threadsTotal} onChange={handleChange} className={styles.input} />
+            <input
+              type="number"
+              name="threadsTotal"
+              value={formData.threadsTotal}
+              onChange={handleChange}
+              className={styles.input}
+            />
           </label>
           <label className={styles.label}>
             Kraken2 Database:
-            <input type="file" name="kraken2Database" value={formData.kraken2Database} onChange={handleChange} className={styles.input} />
+            <input
+              type="file"
+              name="kraken2Database"
+              value={formData.kraken2Database}
+              onChange={handleChange}
+              className={styles.input}
+            />
           </label>
           <label className={styles.label}>
             Krona Database:
-            <input type="file" name="kronaDatabase" value={formData.kronaDatabase} onChange={handleChange} className={styles.input} />
+            <input
+              type="file"
+              name="kronaDatabase"
+              value={formData.kronaDatabase}
+              onChange={handleChange}
+              className={styles.input}
+            />
           </label>
           <label className={styles.inlineLabel}>
             Remove Human Reads:
-            <input type="checkbox" name="removeHumanReads" checked={formData.removeHumanReads} onChange={handleChange} className={styles.checkbox} />
+            <input
+              type="checkbox"
+              name="removeHumanReads"
+              checked={formData.removeHumanReads}
+              onChange={handleChange}
+              className={styles.checkbox}
+            />
           </label>
           <label className={styles.inlineLabel}>
             Remove Unclassified Reads:
-            <input type="checkbox" name="removeUnclassifiedReads" checked={formData.removeUnclassifiedReads} onChange={handleChange} className={styles.checkbox} />
+            <input
+              type="checkbox"
+              name="removeUnclassifiedReads"
+              checked={formData.removeUnclassifiedReads}
+              onChange={handleChange}
+              className={styles.checkbox}
+            />
           </label>
           <label className={styles.label}>
             Adapters:
-            <input type="file" name="adapters" value={formData.adapters} onChange={handleChange} className={styles.input} />
+            <input
+              type="file"
+              name="adapters"
+              value={formData.adapters}
+              onChange={handleChange}
+              className={styles.input}
+            />
           </label>
           <label className={styles.label}>
             Minimum Read Length:
-            <input type="number" name="minimumReadLength" value={formData.minimumReadLength} onChange={handleChange} className={styles.input} />
+            <input
+              type="number"
+              name="minimumReadLength"
+              value={formData.minimumReadLength}
+              onChange={handleChange}
+              className={styles.input}
+            />
           </label>
-          <button type="submit" className={styles.button}>Submit</button>
+          <button type="submit" className={styles.button}>
+            Submit
+          </button>
         </form>
       </div>
-      <div className={styles.columnRight}>
-        {/* Empty column for now */}
-      </div>
+      <div className={styles.columnRight}>{/* Empty column for now */}</div>
     </div>
   );
 }
