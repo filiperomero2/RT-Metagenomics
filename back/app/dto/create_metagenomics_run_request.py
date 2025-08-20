71e120de7eae485a6f730772bde59eed217a54b4
Felippe Nacif <felippenacif@gmail.com>
2025-08-20 15:38:05 -0300

    chore: add pydantic dependency and refactor request classes to inherit from BaseModel

--- back/app/dto/create_metagenomics_run_request.py
+++ back/app/dto/create_metagenomics_run_request.py
@@ -1,11 +1,12 @@
 from typing import List
 from entities.enum import DataType
+from pydantic import BaseModel
 
-class CreateMetagenomicsSampleRequest:
+class CreateMetagenomicsSampleRequest(BaseModel):
     name: str
     barcode: str
     
-class CreateMetagenomicsRunRequest:
+class CreateMetagenomicsRunRequest(BaseModel):
     dataType: DataType
     samples: List[CreateMetagenomicsSampleRequest]
     runName: str
