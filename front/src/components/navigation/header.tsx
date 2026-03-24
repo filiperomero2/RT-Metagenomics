c651751ac87a64e7bd6c4504e36f3e3dcbdf2ef1
HiagoMM <hiagomarquesdemedeiros@gmail.com>
2026-03-24 19:27:41 -0300

    feat: add authentication info file, update window state, enhance header component, and modify about page layout

--- front/src/components/navigation/header.tsx
+++ front/src/components/navigation/header.tsx
@@ -1,44 +1,65 @@
-import { Navbar, NavbarBrand, NavbarContent, NavbarItem } from "@heroui/react";
+import {
+  Button,
+  ButtonGroup,
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+} from "@heroui/react";
 import { Link, useRouterState } from "@tanstack/react-router";
-import { ChartPie, Info, Settings } from "lucide-react";
+import { ChartPie, Info, Minus, Settings, Square, X } from "lucide-react";
 import { BackendStatusButton } from "./backend-status-button";
 
 export function Header() {
   const path = useRouterState({ select: (s) => s.location.pathname });
 
   return (
-    <Navbar isBordered maxWidth="full">
-      <NavbarContent className="flex w-full gap-4" justify="center">
-        <NavbarBrand>
-          <NavbarItem className="flex items-center justify-center">
-            <Link to="/">
-              <img className="bg-background h-10 w-10" src="/logo.webp" />
-            </Link>
-          </NavbarItem>
-        </NavbarBrand>
-
-        <NavbarItem isActive={path === "/meta"}>
-          <Link to="/meta" className="text-foreground flex items-center gap-2">
-            <ChartPie size={18} />
-            Meta
+    <Navbar id="header" maxWidth="full" height="2.7rem" className="bg-content1/40">
+      <NavbarBrand>
+        <NavbarItem className="flex items-center justify-center">
+          <Link to="/">
+            <img className="bg-background h-8 w-8" src="/logo.webp" />
           </Link>
         </NavbarItem>
-        <NavbarItem isActive={path === "/about"}>
+      </NavbarBrand>
+      <NavbarContent justify="center">
+        <NavbarItem isActive={path === "/meta"} className="text-xs">
+          <Link to="/meta" className="text-foreground flex items-center gap-2">
+            <ChartPie size={16} />
+            Metagenomics
+          </Link>
+        </NavbarItem>
+        <NavbarItem isActive={path === "/about"} className="text-xs">
           <Link to="/about" className="text-foreground flex items-center gap-2">
-            <Info size={18} />
+            <Info size={16} />
             About
           </Link>
         </NavbarItem>
-        <NavbarItem isActive={path === "/settings"}>
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <BackendStatusButton />
+        <NavbarItem isActive={path === "/settings"} className="text-xs">
           <Link
             to="/settings"
             className="text-foreground flex items-center gap-2"
           >
-            <Settings size={18} />
-            Settings
+            <Settings size={16} />
           </Link>
         </NavbarItem>
-        <BackendStatusButton />
+
+        <NavbarItem className="text-xs">
+          <ButtonGroup size="sm" variant="light" isIconOnly>
+            <Button>
+              <Minus size={16} />
+            </Button>
+            <Button>
+              <Square size={16} />
+            </Button>
+            <Button>
+              <X size={16} />
+            </Button>
+          </ButtonGroup>
+        </NavbarItem>
       </NavbarContent>
     </Navbar>
   );
